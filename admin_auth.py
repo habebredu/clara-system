4425fc3e19c4a383670f80ebc2738156bbd3a213
habebredu <habeb.rizmi@education.nsw.gov.au>
2025-06-23 20:23:08 +1000

    Update admin_auth.py

--- admin_auth.py
+++ admin_auth.py
@@ -9,7 +9,7 @@ def authenticate_admin(credentials: HTTPBasicCredentials = Depends(security)):
     username = credentials.username
     password = credentials.password
 
-    if username != "admin" or "admin" != password:
+    if username != "admin" or password != "admin":
         raise HTTPException(status_code=401, detail="Unauthorized")
 
     return username
