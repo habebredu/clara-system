aee652b713d2c5433ed670b307effaccc1157fca
habebredu <habeb.rizmi@education.nsw.gov.au>
2025-06-27 20:58:02 +1000

    Update unknown_manager.py

--- unknown_manager.py
+++ unknown_manager.py
@@ -4,6 +4,7 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+from google.auth.exceptions import RefreshError
 
 from email.message import EmailMessage
 from email import message_from_bytes
