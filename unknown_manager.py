7e3123adc9dd61b35a3ce1859e095ffb0f43a6a7
habebredu <habeb.rizmi@education.nsw.gov.au>
2025-06-27 19:46:58 +1000

    Update unknown_manager.py

--- unknown_manager.py
+++ unknown_manager.py
@@ -26,13 +26,14 @@ def get_service():
     creds = None
     if os.path.exists("token.json"):
         creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # Refresh tokens if expired
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+        print("Refreshed tokens...")
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
-            creds = flow.run_local_server(port=0)
+        flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+        creds = flow.run_local_server(port=0)
 
     # Save the credentials for the next run
     with open("token.json", "w") as token:
